efcd1967efab24f8d1901445896ee480a14267fb
speed2CZ <GTGTGT@seznam.cz>
2015-10-28 16:55:04 +0100

    Aeon M4 Allow TMD from beginning

--- SCCA_Coop_A04/SCCA_Coop_A04_script.lua
+++ SCCA_Coop_A04/SCCA_Coop_A04_script.lua
@@ -145,7 +145,6 @@ function OnPopulate(scenario)
                                       categories.EXPERIMENTAL +
                                       categories.PRODUCTFA + -- All FA Units
                                       categories.ual0307 +
-                                      categories.uab4201 + categories.urb4201 +
                                       categories.uab4202 + categories.urb4202 +
                                       categories.uab4203 + categories.urb4203 +
                                       categories.uab2303 + categories.urb2303 )
@@ -650,13 +649,11 @@ function StartMission2()
     ScenarioFramework.RemoveRestrictionForAllHumans(
         categories.uab0302 +  -- Aeon T3 Air Factory
         categories.zab9602 +  -- Aeon T3 Support Air Factory
-        categories.uab4201 +  -- Aeon T2 Tactical Missile Defense
         categories.uab2303 +  -- Aeon T2 Artillery Installation
         categories.uaa0304 +  -- Aeon T3 Strategic Bomber
         categories.uab4202 +  -- Aeon T2 Shield Generator
         categories.urb0302 +  -- Cybran T3 Air Factory
         categories.zrb9602 +  -- Cybran Support T3 Air Factory
-        categories.urb4201 +  -- Cybran T2 Tactical Missile Defense
         categories.urb2303 +  -- Cybran T2 Artillery Installation
         categories.ura0304 +  -- Cybran T3 Strategic Bomber
         categories.urb4202    -- Cybran T2 Shield Generator
