3e4e02db91e45e6b73f13401daa2024021bf4e9b
TheKeyBlue <keyblue357@hotmail.com>
2016-07-26 00:03:50 +0200

    Adjust files according too speed2 feedback

---
+++ ThetaCivilianRescue/ThetaCivilianRescue_m1cybran_manual.lua
@@ -0,0 +1,70 @@
+local ScenarioFramework = import('/lua/ScenarioFramework.lua')
+local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua')
+local TCRUtil = import('/maps/ThetaCivilianRescue/ThetaCivilianRescue_CustomFunctions.lua')
+
+---------
+-- Locals
+---------
+local Difficulty = ScenarioInfo.Options.Difficulty
+
+-----------------------------
+-- Cybran M1 West Base Manual
+-----------------------------
+function CybranM1WestBaseDefensePatrols()
+    local platoon
+    
+    -- Patroling interceptors
+    platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'M1_Cybran_Air_Patrol_D' .. Difficulty, 'GrowthFormation')
+    ScenarioFramework.PlatoonPatrolChain(platoon, 'M1_Cybran_Air_Patrol_Chain')
+    
+    -- Patroling landforces
+    platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'M1_Cybran_Land_Patrol_D' .. Difficulty, 'GrowthFormation')
+    ScenarioFramework.PlatoonPatrolChain(platoon, 'M1_Cybran_Land_Patrol_Chain')
+    
+    MMLPlatoon = ScenarioUtils.CreateArmyGroup('Cybran', 'M1_Cybran_Land_MML_D' .. Difficulty, false)
+    TCRUtil.CreateAreaTrigger(MMLAttackACU, 'M1_Cybran_Base_Area' , categories.COMMAND, true, false, 1)
+end
+
+function MMLAttackACU()
+    units = TCRUtil.GetAllCatUnitsInArea(categories.COMMAND, 'M1_Cybran_Base_Area')
+    for _,unit in units do
+        IssueAttack( MMLPlatoon, unit)
+        break
+    end
+end
+
+function CybranM1WestBaseForwardPatrols()
+    local platoon
+    
+    if ScenarioInfo.NumberOfPlayers >= 3 then
+        -- Patroling interceptors
+        if Difficulty >= 1 then 
+            platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'Hunter_S1', 'GrowthFormation')
+            ScenarioFramework.PlatoonPatrolChain(platoon, 'M1_Forward_S1_Patrol_Chain')
+            
+            platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'Mixed_S2', 'GrowthFormation')
+            ScenarioFramework.PlatoonPatrolChain(platoon, 'M1_Forward_S2_Patrol_Chain')
+        end
+        if Difficulty >= 2 then 
+            ForkThread(function()
+                WaitSeconds(30) -- 30seconds
+                platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'Medusa_S3', 'GrowthFormation')
+                ScenarioFramework.PlatoonAttackChain(platoon, 'M1_Forward_S3_Attack_Chain')
+            
+            end)
+            
+            platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'Mixed_S4', 'GrowthFormation')
+            ScenarioFramework.PlatoonPatrolChain(platoon, 'M1_Forward_S4_Patrol_Chain')
+        end
+        if Difficulty >= 3 then 
+            
+            ForkThread(function()
+                WaitSeconds(600) -- 10min0sec
+                -- Dialog?
+                platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'Delayed_Assault_S6', 'GrowthFormation')
+                ScenarioFramework.PlatoonAttackChain(platoon, 'M1_Forward_S6_Attack_Chain')
+            
+            end)
+        end
+    end
+end
---
+++ ThetaCivilianRescue/ThetaCivilianRescue_m2cybran_manual.lua
@@ -0,0 +1,143 @@
+local ScenarioFramework = import('/lua/ScenarioFramework.lua')
+local Objectives = import('/lua/ScenarioFramework.lua').Objectives
+local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua')
+local TCRUtil = import('/maps/ThetaCivilianRescue/ThetaCivilianRescue_CustomFunctions.lua')
+
+---------
+-- Locals
+---------
+local Difficulty = ScenarioInfo.Options.Difficulty
+
+local spawnMonkeylords = 0
+local spawnMegaliths = 0
+local spawnCorsairs = 0
+local singleMega = false
+local timeBeforeFocus = 5
+
+function CybranM2EastBaseDefensePatrols()
+    local platoon
+    
+    -- Patroling interceptors
+    platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'M2_Cybran_Air_Patrol_D' .. Difficulty, 'GrowthFormation')
+    ScenarioFramework.PlatoonPatrolChain(platoon, 'M2_Cybran_Air_Patrol_Chain')
+    
+    -- Patroling landforces
+    platoon = ScenarioUtils.CreateArmyGroupAsPlatoon('Cybran', 'M2_Cybran_Land_Patrol_D' .. Difficulty, 'GrowthFormation')
+    ScenarioFramework.PlatoonPatrolChain(platoon, 'M2_Cybran_Land_Patrol_Chain')
+end
+
+----------------------
+-- Drop units at start
+----------------------
+function CybranM2EastBaseUnitDrops()
+    TCRUtil.DropUnits('Rhino_1_D' .. Difficulty, 'M1_Forward_S1_Patrol_Chain_0', 'Transport_End_Point', 'M1_Forward_S1_Patrol_Chain')
+    TCRUtil.DropUnits('Loya_3_D' .. Difficulty, 'M2_Cybran_Drop_Loya_Chain_3_0', 'Transport_End_Point', 'M2_Cybran_Drop_Loya_Chain_3')
+    TCRUtil.DropUnits('Loya_4_D' .. Difficulty, 'M2_Cybran_Drop_Loya_Chain_4_0', 'Transport_End_Point', 'M2_Cybran_Drop_Loya_Chain_4')
+    
+    if Objectives.IsComplete(ScenarioInfo.M1S1) then
+        TCRUtil.DropUnits('Rhino_2_D' .. Difficulty, 'M2_Cybran_Drop_Rhino_Chain_2_0', 'Transport_End_Point', 'M2_Cybran_Drop_Rhino_Chain_2')
+        TCRUtil.DropUnits('Loya_5_D' .. Difficulty, 'M2_Cybran_Drop_Loya_Chain_5_0', 'Transport_End_Point', 'M2_Cybran_Drop_Loya_Chain_5')
+    else
+        TCRUtil.DropUnits('Rhino_2_D' .. Difficulty, 'M2_Cybran_Drop_Rhino_Alternative', 'Transport_End_Point', 'M1_Forward_S1_Patrol_Chain')
+        TCRUtil.DropUnits('Loya_5_D' .. Difficulty, 'M2_Cybran_Drop_Loya_Alternative', 'Transport_End_Point', 'M2_Cybran_Drop_Loya_Chain_4')
+    end
+end
+
+----------------------
+--Spawn Experimentals
+----------------------
+
+function DropExperimental(deathCallback)
+    if spawnMonkeylords <= spawnMegaliths then
+        spawnMonkeylords = spawnMonkeylords + 1
+    else
+        spawnMegaliths = spawnMegaliths + 1
+    end
+
+    --some changes to have a better increase of difficulty.
+    if spawnMonkeylords == 1 and spawnMegaliths == 1 and (not singleMega) then
+        spawnMonkeylords = 0
+        singleMega = true
+    end
+
+    for i = 1,spawnMonkeylords do
+        local units = {}
+        local move1 = math.random(10)
+        local move2 = math.random(10)
+        local monkeylord = CreateUnitHPR("url0402", "Cybran", 240 + move1, 6.75, 10 + move2,0,0,0)
+        ScenarioFramework.CreateUnitDeathTrigger(deathCallback, monkeylord)
+        table.insert(units, monkeylord )
+        local transport = CreateUnitHPR("ura0104", "Cybran", 240 + move1, 6.75, 10 + move2,0,0,0)
+        table.insert(units, transport )
+        TCRUtil.DropUnits(units, 'M2_Exp_Drop_Location', 'Transport_End_Point', 'M2_Cybran_Exp_Attack_Chain_1')
+        FocusACUs(monkeylord)
+    end
+    for i = 1,spawnMegaliths do
+        local units = {}
+        local move1 = math.random(10)
+        local move2 = math.random(10)
+        local megalith = CreateUnitHPR("xrl0403", "Cybran", 240 + move1, 6.75, 10 + move2,0,0,0)
+        ScenarioFramework.CreateUnitDeathTrigger(deathCallback, megalith)
+        table.insert(units, megalith )
+        local transport = CreateUnitHPR("ura0104", "Cybran", 240 + move1, 6.75, 10 + move2,0,0,0)
+        table.insert(units, transport )
+        TCRUtil.DropUnits(units, 'M2_Exp_Drop_Location', 'Transport_End_Point', 'M2_Cybran_Exp_Attack_Chain_1')
+        FocusACUs(megalith)
+    end
+end
+
+function FocusACUs(unit)
+    TCRUtil.CreateFocusACUTrigger( unit, ScenarioInfo.PlayerCDR, 30, timeBeforeFocus )
+    for i = 2,ScenarioInfo.NumberOfPlayers do
+        TCRUtil.CreateFocusACUTrigger( unit, ScenarioInfo.CoopCDR[i - 1], 30 , timeBeforeFocus)
+    end
+end
+
+function AfterExperimentalDrops()
+    -- 2 times 2 loyalists
+    local dropLocations = {'M1_Forward_S1_Patrol_Chain_0', 'M2_Cybran_Drop_Rhino_Chain_2_0'}
+    local attackChains = {'M1_Forward_S1_Patrol_Chain', 'M2_Cybran_Drop_Rhino_Chain_2'}
+    for i = 1,2 do
+        local units = {}
+        for i = 1,2 do
+            local loyalist = CreateUnitHPR("url0303", "Cybran", 250, 6.75, 15,0,0,0)
+            table.insert(units, loyalist )
+        end
+        local transport = CreateUnitHPR("ura0104", "Cybran", 250, 6.75, 15,0,0,0)
+        table.insert(units, transport )
+        TCRUtil.DropUnits(units, dropLocations[i], 'Transport_End_Point', attackChains[i])
+    end
+    
+    -- 3 times 1 brick
+    dropLocations = {'M2_Cybran_Drop_Loya_Chain_3_0', 'M2_Cybran_Drop_Loya_Chain_4_0', 'M2_Cybran_Drop_Loya_Chain_5_0'}
+    attackChains = {'M2_Cybran_Drop_Loya_Chain_3', 'M2_Cybran_Drop_Loya_Chain_4', 'M2_Cybran_Drop_Loya_Chain_5'}
+    for i = 1,3 do
+        local units = {}
+        local brick = CreateUnitHPR("xrl0305", "Cybran", 250, 6.75, 15,0,0,0)
+        table.insert(units, brick )
+        local transport = CreateUnitHPR("ura0104", "Cybran", 250, 6.75, 15,0,0,0)
+        table.insert(units, transport )
+        TCRUtil.DropUnits(units, dropLocations[i], 'Transport_End_Point', attackChains[i])
+    end
+end
+
+function SnipeACUs()
+    local units = TCRUtil.GetAllCatUnitsInArea(categories.COMMAND, 'M2_Area')
+    spawnCorsairs = spawnCorsairs + 5
+    if spawnCorsairs < 21 then
+        for _, cdr in units do
+            for i = 1,spawnCorsairs do
+                local corsair = CreateUnitHPR("dra0202", "Cybran", 245 + math.random(10), 6.75, 10 + math.random(10),0,0,0)
+                IssueAttack({corsair}, cdr)
+            end
+        end
+    else
+        for _, cdr in units do
+            local spawnStrats = spawnCorsairs / 5
+            for i = 1,spawnStrats do
+                local strat = CreateUnitHPR("ura0304", "Cybran", 245 + math.random(10), 6.75, 10 + math.random(10),0,0,0)
+                IssueAttack({strat}, cdr)
+            end
+        end
+    end
+end
