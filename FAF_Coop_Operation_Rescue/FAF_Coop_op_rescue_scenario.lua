9b8a5128c8d1995ac58f00a443308f3a69719f4b
JJ173 <jack.t371@hotmail.co.uk>
2017-12-16 16:30:13 +0000

    New Mission: Operation Rescue
    This is my first completed mission. It's not perfect, and it could probably do with some polishing. However, I want to make this live for feedback purposes. Players are able to complete it as well as fail. I would like opinions on difficulty, AI setting and the general idea. Hopefully with this creation published, I can achieve that.

---
+++ FAF_Coop_Operation_Rescue/FAF_Coop_op_rescue_scenario.lua
@@ -0,0 +1,22 @@
+version = 3
+ScenarioInfo = {
+    name = 'Operation Rescue',
+    description = 'A group of UEF scientists are trapped on a planet and a Cybran Commander gated near them quite a while back. They managed to send a distress signal before the Cybran built a long-range Jammer. We\'ve managed to locate the scientists. Commander, you will gate in and help those scientists.',
+    type = 'campaign_coop',
+    starts = true,
+    preview = '',
+    size = {1024, 1024},
+    map = '/maps/FAF_Coop_Operation_Rescue/FAF_Coop_op_rescue.scmap',
+    save = '/maps/FAF_Coop_Operation_Rescue/FAF_Coop_op_rescue_save.lua',
+    script = '/maps/FAF_Coop_Operation_Rescue/FAF_Coop_op_rescue_script.lua',
+    norushradius = 0.000000,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                { name = 'FFA', armies = {'Player1','Cybran','UEF','Player2','Player3','Player4',} },
+            },
+            customprops = {
+            },
+            factions = { {'uef'}, {'uef'}, {'uef'}, {'uef'} },
+        },
+    }}
---
+++ FAF_Coop_Operation_Rescue/M2_Cybran_Jammer_2_AI.lua
@@ -0,0 +1,200 @@
+local BaseManager = import('/lua/ai/opai/basemanager.lua')
+local SPAIFileName = '/lua/scenarioplatoonai.lua'
+
+local Cybran = 2
+
+local Difficulty = ScenarioInfo.Options.Difficulty
+
+local M2CybranBase2 = BaseManager.CreateBaseManager()
+
+function M2_Cybran_Base_Function()
+    M2CybranBase2:Initialize(ArmyBrains[Cybran], 'JammerBase2', 'JammerBase2', 50, {JammerBase2 = 100})
+    M2CybranBase2:StartNonZeroBase(8, 5)
+
+    M2CybranBase2:SetActive('AirScouting', true)
+
+    M2_Cybran_Air_Attacks()
+    M2_Cybran_Transport_Attacks()
+end
+
+function M2_Cybran_Air_Attacks()
+    local opai = nil
+    local quantity = {}
+    local trigger = {}
+
+    -- Initial Attacks
+    quantity = {2, 4, 6}
+    opai = M2CybranBase2:AddOpAI('AirAttacks', 'M2_JammerAirAttack_2_1',
+        {
+            MasterPlatoonFunction = {SPAIFileName, 'PatrolThread'},
+            PlatoonData = {
+                PatrolChain = 'Cybran_Jammer_Air_Attack_2'
+            },
+            Priority = 250,
+        }
+    )
+    opai:SetChildQuantity('Bombers', quantity[Difficulty])
+
+    quantity = {8, 12, 16}
+    opai = M2CybranBase2:AddOpAI('AirAttacks', 'M2_JammerAirAttack_2_2',
+        {
+            MasterPlatoonFunction = {SPAIFileName, 'PatrolThread'},
+            PlatoonData = {
+                PatrolChain = 'Cybran_Jammer_Air_Attack_2'
+            },
+            Priority = 225,
+        }
+    )
+    opai:SetChildQuantity('Interceptors', quantity[Difficulty])
+
+    quantity = {2, 3, 4}
+    opai = M2CybranBase2:AddOpAI('AirAttacks', 'M2_JammerAirAttack_2_3',
+        {
+            MasterPlatoonFunction = {SPAIFileName, 'PatrolThread'},
+            PlatoonData = {
+                PatrolChain = 'Cybran_Jammer_Air_Attack_2'
+            },
+            Priority = 225,
+        }
+    )
+    opai:SetChildQuantity('LightGunships', quantity[Difficulty])
+end
+
+function M2_Cybran_Transport_Attacks()
+    -- We want this AI to mainly be made up of transport drops.
+    local opai = nil
+    local trigger = {}
+
+    -- Transport Builder
+    opai = M2CybranBase2:AddOpAI('EngineerAttack', 'M2_Cybran_TransportBuilder',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 1000,
+    })
+    opai:SetChildQuantity('T2Transports', 2)
+    opai:SetLockingStyle('None')
+    opai:AddBuildCondition('/lua/editor/unitcountbuildconditions.lua',
+        'HaveLessThanUnitsWithCategory', {'default_brain', 8, categories.uaa0104})
+
+    -- Tech 1
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack1',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 150,
+    })
+    opai:SetChildQuantity('HeavyBots', 12)
+
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack2',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 200,
+    })
+    opai:SetChildQuantity('LightBots', 16)
+
+    trigger = {32, 26, 20}
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack3',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 175,
+    })
+    opai:SetChildQuantity('HeavyBots', 16)
+    opai:AddBuildCondition('/lua/editor/otherarmyunitcountbuildconditions.lua',
+        'BrainsCompareNumCategory', {'default_brain', {'HumanPlayers'}, trigger[Difficulty], categories.MOBILE, '>='})
+
+    -- Tech 2
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack4',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 200,
+    })
+    opai:SetChildQuantity('HeavyTanks', 8)
+    opai:AddBuildCondition('/lua/editor/otherarmyunitcountbuildconditions.lua',
+        'BrainsCompareNumCategory', {'default_brain', {'HumanPlayers'}, 1, categories.FACTORY * categories.TECH2, '>='})
+
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack5',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 240,
+    })
+    opai:SetChildQuantity('HeavyTanks', 10)
+    opai:AddBuildCondition('/lua/editor/otherarmyunitcountbuildconditions.lua',
+        'BrainsCompareNumCategory', {'default_brain', {'HumanPlayers'}, 2, categories.FACTORY * categories.TECH2, '>='})
+
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack6',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 245,
+    })
+    opai:SetChildQuantity('MobileMissiles', 10)
+    opai:AddBuildCondition('/lua/editor/otherarmyunitcountbuildconditions.lua',
+        'BrainsCompareNumCategory', {'default_brain', {'HumanPlayers'}, 2, categories.FACTORY * categories.TECH2, '>='})
+
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack7',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 250,
+    })
+    opai:SetChildQuantity('MobileFlak', 6)
+    opai:AddBuildCondition('/lua/editor/otherarmyunitcountbuildconditions.lua',
+        'BrainsCompareNumCategory', {'default_brain', {'HumanPlayers'}, 1, categories.FACTORY * categories.TECH2, '>='})
+
+    trigger = {28, 22, 16}
+    opai = M2CybranBase2:AddOpAI('BasicLandAttack', 'M2_CybranTransportAttack8',
+    {
+        MasterPlatoonFunction = {'/lua/ScenarioPlatoonAI.lua', 'LandAssaultWithTransports'},
+        PlatoonData = {
+            AttackChain = 'M2_Cybran_Tran_Attack_Chain',
+            LandingChain = 'M2_Cybran_Tran_Land_Chain',
+            TransportReturn = 'M2_Transport_Return_Marker',
+        },
+        Priority = 175,
+    })
+    opai:SetChildQuantity('HeavyTanks', 8)
+    opai:AddBuildCondition('/lua/editor/otherarmyunitcountbuildconditions.lua',
+        'BrainsCompareNumCategory', {'default_brain', {'HumanPlayers'}, trigger[Difficulty], categories.MOBILE, '>='})
+end
+
+function DisableBase()
+    if(M2CybranBase2) then
+        M2CybranBase2:BaseActive(false)
+    end
+end
+
