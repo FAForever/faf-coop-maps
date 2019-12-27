6ddd4e113a373cf843b867d0e15b12e64023df74
shadowlorda1 <shadowlorda1@gmail.com>
2019-12-27 02:29:16 -0700

    Mission 2 Config changes

--- FAF_Coop_Operation_Uhthe_Thuum_QAI/FAF_Coop_Operation_Uhthe_Thuum_QAI_scenario.lua
+++ FAF_Coop_Operation_Uhthe_Thuum_QAI/FAF_Coop_Operation_Uhthe_Thuum_QAI_scenario.lua
@@ -1,7 +1,7 @@
 version = 3
 ScenarioInfo = {
     name = 'Seraphim Mission 3: Operation Uhthe-Thuum-QAI',
-    description = 'QAI survived and needs our help to reupload himself into the Quantum network, you will gate in and support him in any way you can. He alone has such vast processing power he is vital to our struggle against the Humans.',
+    description = 'QAI survived and needs our help to reupload himself into the Quantum network, you will gate in and support him in any way you can. He alone has such vast processing power he is vital to our plan against the Humans.',
     type = 'campaign_coop',
     starts = true,
     preview = '',
@@ -14,10 +14,10 @@ ScenarioInfo = {
     Configurations = {
         ['standard'] = {
             teams = {
-                { name = 'FFA', armies = {'Cybran1','QAI','Nodes','Cybran2','Player1','Player2','Player4','Player3',} },
+                { name = 'FFA', armies = {'Player1','Cybran1','QAI','Nodes','Cybran2','Player2','Player4','Player3',} },
             },
             customprops = {
             },
-			factions = { {'seraphim'}, {'seraphim'}, {'seraphim'}, {'seraphim'} },
+			factions = { {'seraphim'}, {'aeon', 'seraphim'}, {'aeon', 'seraphim'}, {'aeon', 'seraphim'} },
         },
     }}
