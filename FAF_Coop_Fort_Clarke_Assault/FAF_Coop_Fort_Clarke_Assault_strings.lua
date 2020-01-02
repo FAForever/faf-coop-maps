e5e03917a6e5093a2d456e7ecaa1e10865c217b3
speed2 <GTGTGT@seznam.cz>
2020-01-02 17:43:38 +0100

    Tabs to spaces

--- FAF_Coop_Fort_Clarke_Assault/FAF_Coop_Fort_Clarke_Assault_strings.lua
+++ FAF_Coop_Fort_Clarke_Assault/FAF_Coop_Fort_Clarke_Assault_strings.lua
@@ -50,14 +50,14 @@
 -- Win/Lose
 -----------
 PlayerWin = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 
 
 -- PO1 Factory West Destroyed / Actor:  / Update: 24/2/2016 / VO TODO
 BaseDestroyed = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 
@@ -67,17 +67,17 @@ BaseDestroyed = {
 ------------
 -- Assign objective to kill Civilian City / Actor:  / Update: 24/2/2016 / VO TODO
 M2_Kill_Civs_Objective = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 -- Objective to Destroy M2 UEF City complete / Actor:  / Update: 24/2/2016 / VO TODO
 M2_Civs_Killed = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 -- Order Commander Killed / Actor:  / Update: 24/2/2016 / VO TODO
 M2_OrderCommanderKilled = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 
@@ -86,7 +86,7 @@ M2_OrderCommanderKilled = {
 ------------
 -- Experimentals from Counterattack killed / Actor:  / Update: 24/2/2016 / VO TODO
 M3_All_Exps_Killed = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 
@@ -97,31 +97,31 @@ M3_All_Exps_Killed = {
 
 -- Ythothas and T2 subs for player / Actor:  / Update: 24/2/2016 / VO TODO
 M4_Reinforcements = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 
 -- Fork Clarke Destroyed / Actor:  / Update: 24/2/2016 / VO TODO
 M4_Fort_Clarke_Destroyd = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 -- Aeon ACU Killed / Actor:  / Update: 24/2/2016 / VO TODO
 M4_Aeon_ACU_Killed = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 -- Cybran ACU Killed / Actor:  / Update: 24/2/2016 / VO TODO
 M4_Cybran_ACU_Killed = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 -- UEF ACU Killed / Actor:  / Update: 24/2/2016 / VO TODO
 M4_UEF_ACU_Killed = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
 
 -- All ACUs Killed / Actor:  / Update: 24/2/2016 / VO TODO
 M4_All_ACUs_Killed = {
-  {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
+    {text = '[Seraphim]: Something.', vid = '', bank = '', cue = '', faction = 'Seraphim'},
 }
