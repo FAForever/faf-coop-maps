d8e274ada44ab20bcbe1da91488eff1edd1f1a79
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-04-20 17:49:20 +0200

    Delete the old map

--- FAF_Coop_Operation_Rebel's_Rest/FAF_Coop_Operation_Rebel's_Rest_operation.lua
+++
@@ -1,9 +0,0 @@
-local OpStrings = import('/maps/FAF_Coop_Operation_Rebel\'s_Rest/FAF_Coop_Operation_Rebel\'s_Rest_strings.lua')
-
-operationData = 
-{
-    key = 'OperationRebel\'sRest',
-    feedbackURL = 'https://discord.gg/ayzAVr9JUV',
-	opDebriefingSuccess = OpStrings.Debriefing_Win,
-    opDebriefingFailure = OpStrings.Debriefing_Lose,
-}
