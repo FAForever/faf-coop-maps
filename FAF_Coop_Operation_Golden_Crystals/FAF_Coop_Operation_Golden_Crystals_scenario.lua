a8d0913dc876f7bcdaacba50161b3d2d9802abf1
Yuchen <116278150+Yuchenjimmy@users.noreply.github.com>
2025-03-03 14:49:27 +0800

    Remove LOC entries from FAF_Coop_Operation_Golden_Crystals_scenario.lua

--- FAF_Coop_Operation_Golden_Crystals/FAF_Coop_Operation_Golden_Crystals_scenario.lua
+++ FAF_Coop_Operation_Golden_Crystals/FAF_Coop_Operation_Golden_Crystals_scenario.lua
@@ -1,7 +1,7 @@
 version = 3
 ScenarioInfo = {
-    name = '<LOC FAF_Coop_Operation_Golden_Crystals_Name>Coalition Mission 3 :Operation Golden Crystals',
-    description = '<LOC FAF_Coop_Operation_Golden_Crystals_Description>We lost contact with an important Mining facility in the outer rim. Your team is being sent to investigate.',
+    name = 'Coalition Mission 3 :Operation Golden Crystals',
+    description = 'We lost contact with an important Mining facility in the outer rim. Your team is being sent to investigate.',
     type = 'campaign_coop',
     starts = true,
     preview = '',
